f59e326d65259266c5a6ec39142b8754c0642cdc
AmrFathyMohamed <amr.fathy27196@gmail.com>
2023-10-07 15:14:33 +0300

    Push

--- checksearch.php
+++ checksearch.php
@@ -4,100 +4,101 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Include the Database class
     require_once 'classes/Database.php';
-
-    // Initialize an empty array to store conditions
-    $conditions = [];
-
-    $date = $_POST['date'];
-    if ($date !== null) {
-        $conditions[] = "appointments.Date >= '$date'";
-    }
-
+    // Get form input values
+    //$query = $_POST['query'];
+    $date = isset($_POST['date']) ? $_POST['date'] : '';
     $gendermale = isset($_POST['GenderM']) ? 1 : 0;
     $genderfemale = isset($_POST['GenderF']) ? 1 : 0;
-    if ($gendermale && $genderfemale) {
-        $conditions[] = "Gender IN ('Male', 'Female')";
-    } elseif ($gendermale) {
-        $conditions[] = "Gender = 'Male'";
-    } elseif ($genderfemale) {
-        $conditions[] = "Gender = 'Female'";
-    }
-
     $speciality = $_POST['speciality'];
-    if ($speciality != 'All') {
-        $conditions[] = "Specialization = '$speciality'";
-    }
-
     $priceRange = $_POST['priceRange'];
-    if ($priceRange !== null) {
-        $conditions[] = "PriceAfterPercentage > $priceRange";
-    }
-
-    // Check if there are any conditions to add to the SQL query
-    if (!empty($conditions)) {
-        // Build the SQL query based on the conditions
-        $sql = "SELECT therapists.*, GROUP_CONCAT(appointments.Date) AS appointment_dates
-                FROM therapists
-                INNER JOIN appointments ON therapists.TherapistID = appointments.TherapistID AND appointments.Type = 'Work'
-                WHERE (
-                    (
-                        (SELECT COUNT(*) FROM sessions WHERE sessions.TherapistID = therapists.TherapistID) = 0
-                    )";
-
-        // Add the conditions to the query
-        $sql .= " AND " . implode(" AND ", $conditions);
-
-        $sql .= ") GROUP BY therapists.TherapistID  ORDER BY therapists.FullName ASC";
-
-        // Execute the query
-        $db = new Database();
-        $result = $db->executeQuery($sql);
-
-        if (!$result) {
-            die("Query error: " . $conn->error);
-        }
-
-        // Fetch and display the results
-        while ($row = $result->fetch_assoc()) {
-            $html = '<div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">';
-            $html .= '<div class="team-item rounded">';
-            $html .= '<img class="img-fluid" src="' . $row["Profile"] . '" alt="" />';
-            $html .= '<div class="text-center p-4">';
-            $html .= '<h5 class="text-">' . $row["FullName"] . '</h5>';
-            $html .= '<span>' . $row["Specialization"] . '</span>';
-            $html .= '</div>';
-            $html .= '<div class="team-text text-center bg-white p-4">';
-            $html .= '<h5>' . $row["FullName"] . '</h5>';
-            $html .= '<p>' . $row["Specialization"] . '</p>';
-
-            // Display the rating stars
-            $html .= '<div class="rate d-flex justify-content-center align-content-center mb-2">';
-            $rating = $row["Rating"];
-            for ($i = 1; $i <= 5; $i++) {
-                if ($i <= $rating) {
-                    $html .= '<i class="fa-solid fa-star text-warning"></i>';
-                } else {
-                    $html .= '<i class="fa-regular fa-star text-warning"></i>';
-                }
-            }
-            $html .= '</div>';
-
-            $html .= '<div class="price d-flex justify-content-center align-content-center">';
-            $html .= '<p>' . $row["PriceAfterPercentage"] . ' / ساعة</p>';
-            $html .= '<i class="fa-solid fa-money-bill-1-wave ms-2"></i>';
-            $html .= '</div>';
-
-            $html .= '<a class="btn w-100 btn-light m-1" href="therapist-profile.php?id=' . $row["TherapistID"] . '">عرض الملف الشخصي</a>';
-
-            $html .= '</div>';
-            $html .= '</div>';
-            $html .= '</div>';
-            $html .= '</div>';
-
-            echo $html;
-        }
+    if ($date == '') {
+        $sql = "SELECT therapists.* FROM therapists WHERE (1=1 ";
     } else {
-        echo "No search criteria provided.";
+        $sql = "SELECT therapists.*, GROUP_CONCAT(appointments.Date) AS appointment_dates
+        FROM therapists
+        INNER JOIN appointments ON therapists.TherapistID = appointments.TherapistID AND appointments.Date >= '$date' AND appointments.Type = 'Work'
+        WHERE (
+            (
+                (SELECT COUNT(*) FROM sessions WHERE sessions.TherapistID = therapists.TherapistID AND sessions.Date >= '$date') = 0
+            )";
     }
+    // Build the SQL query based on the form inputs
+
+    if ($gendermale && $genderfemale) {
+        $sql .= " AND Gender IN ('Male', 'Female')";
+    } elseif ($gendermale) {
+        $sql .= " AND Gender = 'Male'";
+    } elseif ($genderfemale) {
+        $sql .= " AND Gender = 'Female'";
+    }
+
+    if ($speciality != 'All') {
+        $sql .= " AND Specialization = '$speciality'";
+    }
+
+    if ($priceRange) {
+        $sql .= " AND PriceAfterPercentage > $priceRange";
+    }
+
+    $sql .= ") GROUP BY therapists.TherapistID  ORDER BY therapists.FullName ASC";
+
+    // Execute the query
+    //echo $sql;
+    $db = new Database();
+
+    // Attempt to log in the user
+    $result = $db->executeQuery($sql);
+
+    if (!$result) {
+        die("Query error: " . $conn->error);
+    }
+
+    // Fetch and display the results
+    // echo $sql;
+    // echo "<br>";
+
+    while ($row = $result->fetch_assoc()) {
+        $html = '<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">';
+        $html .= '<div class="team-item rounded">';
+        $html .= '<img class="img-fluid" src="' . $row["Profile"] . '" alt="" />';
+        $html .= '<div class="text-center p-4">';
+        $html .= '<h5 class="text-">' . $row["FullName"] . '</h5>';
+        $html .= '<span>' . $row["Specialization"] . '</span>';
+        $html .= '</div>';
+        $html .= '<div class="team-text text-center bg-white p-4">';
+        $html .= '<h5>' . $row["FullName"] . '</h5>';
+        $html .= '<p>' . $row["Specialization"] . '</p>';
+
+        // Display the rating stars
+        $html .= '<div class="rate d-flex justify-content-center align-content-center mb-2">';
+        $rating = $row["Rating"];
+        for ($i = 1; $i <= 5; $i++) {
+            if ($i <= $rating) {
+                $html .= '<i class="fa-solid fa-star text-warning"></i>';
+            } else {
+                $html .= '<i class="fa-regular fa-star text-warning"></i>';
+            }
+        }
+        $html .= '</div>';
+
+        $html .= '<div class="price d-flex justify-content-center align-content-center">';
+        $html .= '<p>' . $row["PriceAfterPercentage"] . ' / ساعة</p>';
+        $html .= '<i class="fa-solid fa-money-bill-1-wave ms-2"></i>';
+        $html .= '</div>';
+
+        $html .= '<a class="btn w-100 btn-light m-1" href="therapist-profile.php?id=' . $row["TherapistID"] . '">عرض الملف الشخصي</a>';
+
+        $html .= '</div>';
+        $html .= '</div>';
+        $html .= '</div>';
+        $html .= '</div>';
+
+
+        echo $html;
+    }
+
+
+    // Close the database connection
+    //$conn->close();
 }
 ?>
--- search.php
+++ search.php
@@ -148,15 +148,15 @@ $alltherapists = $therapistTable2->getTherapists();
 <!-- Team End -->
 <?php include("includes/footer.php"); ?>
 <script>
-  function getCurrentDate() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = (now.getMonth() + 1).toString().padStart(2, '0');
-    const day = now.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
-  // Set the input's value to the current date
-  document.getElementById('date').value = getCurrentDate();
+  // function getCurrentDate() {
+  //   const now = new Date();
+  //   const year = now.getFullYear();
+  //   const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  //   const day = now.getDate().toString().padStart(2, '0');
+  //   return `${year}-${month}-${day}`;
+  // }
+  // // Set the input's value to the current date
+  // document.getElementById('date').value = getCurrentDate();
   function searchTherapists() {
     var searchQuery = document.getElementById("search").value.toLowerCase().trim();
     var therapistsList = document.getElementById("therapistsList");
@@ -179,7 +179,7 @@ $alltherapists = $therapistTable2->getTherapists();
   }
   function addTherapistToResults(parentElement, therapist) {
     const therapistDiv = document.createElement('div');
-    therapistDiv.className = 'col-lg-3 col-md-6 wow fadeInUp';
+    therapistDiv.className = 'col-lg-4 col-md-6 wow fadeInUp';
     therapistDiv.setAttribute('data-wow-delay', '0.1s');
     therapistDiv.innerHTML = `<div class="team-item rounded">
                         <img class="img-fluid" src="${therapist['Profile']}" alt="" />
